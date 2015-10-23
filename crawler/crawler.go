2e1b8af081187b2984761df4f62653af33df399a
Marvin Qian <fueledbymarvin@gmail.com>
2015-10-23 15:46:10 -0400

    Setup basic crawler methods
    Currently not actually crawling pages. Method stubbed.

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -9,6 +9,16 @@ import (
 	"github.com/fueledbymarvin/gocardless/logs"
 )
 
+type Sitemap struct {
+	Host string
+	Nodes map[*url.URL]*Node
+}
+
+type Node struct {
+	URL *url.URL
+	Neighbors []*Node
+}
+
 func Parse(uStr string) (*url.URL, error) {
 	u, err := url.Parse(uStr)
 	if err != nil {
@@ -22,7 +32,63 @@ func Parse(uStr string) (*url.URL, error) {
 	return u, nil
 }
 
-func Crawl(u *url.URL) (string, error) {
+func Crawl(u *url.URL) ([]*Node, error) {
 
-	return "placeholder", nil
+	sitemap := &Sitemap{Host: u.Host, Nodes: make(map[*url.URL]*Node)}
+	sitemap.Nodes[u] = &Node{URL: u, Neighbors: make([]*Node, 0)}
+
+	urls := make([]*url.URL, 0)
+	urls = append(urls, u)
+	for len(urls) > 0 {
+		var toCrawl *url.URL
+		toCrawl, urls = urls[0], urls[1:len(urls)]
+		links := sitemap.process(toCrawl)
+		urls = append(urls, links...)
+	}
+	
+	return sitemap.asSlice(), nil
 }
+
+func (this *Sitemap) process(u *url.URL) []*url.URL {
+	
+	links := getLinks(u)
+
+	// Only return unseen links with the given Host
+	newLinks := make([]*url.URL, len(links))
+	for _, link := range(links) {
+		if _, seen := this.Nodes[link]; !seen && link.Host == this.Host {
+			newLinks = append(newLinks, link)
+		}
+	}
+
+	// Create links between original node and discovered links
+	node :=	this.Nodes[u]
+	for _, link := range(links) {
+		var linkedNode *Node
+		var seen bool
+		if linkedNode, seen = this.Nodes[link]; !seen {
+			linkedNode := &Node{URL: link, Neighbors: make([]*Node, 1)}
+			this.Nodes[link] = linkedNode
+		}
+		linkedNode.Neighbors = append(linkedNode.Neighbors, node)
+		node.Neighbors = append(node.Neighbors, linkedNode)
+	}
+
+	return newLinks
+}
+
+func (this *Sitemap) asSlice() []*Node {
+
+	nodes := make([]*Node, 0)
+	for _, node := range(this.Nodes) {
+		nodes = append(nodes, node)
+	}
+	
+	return nodes
+}
+
+func getLinks(u *url.URL) []*url.URL {
+
+	return make([]*url.URL, 0)
+}
+
